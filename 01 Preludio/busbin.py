a23da3e1a95345bc670f868b668f7c83e963a5e4
fcpietra <fpietra@gmail.com>
2023-09-02 12:04:48 -0300

    agrego busbin

--- 01 Preludio/busbin.py
+++ 01 Preludio/busbin.py
@@ -1,4 +1,5 @@
 def busbin(T, ini, fin, x):
+    print("vector", T, "inicio", ini, "fin", fin, "elemento", x)
     if(ini == fin):
         if(T[ini]==x):
             return ini
@@ -7,9 +8,11 @@ def busbin(T, ini, fin, x):
     else:
         mid = (ini+fin)//2
         if(x < T [mid]):
-            return busbin(T, ini, mid - 1, x)
+            return busbin(T, ini, mid-1, x)
         else:
-            return busbin(T, mid, fin, x)
+            return busbin(T, mid+1, fin, x)
 
-print(busbin([26,28,36,64], 0, 3, 64))
+##TEST
+print(busbin([26,28,36,64,67,102], 0, 3, 64))
+
     
